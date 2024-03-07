0c90d96c39dbc25d5431f05bb7ea4023b52f58d8
divyaansh0105 <158050858+divyaansh0105@users.noreply.github.com>
2024-03-07 12:05:28 +0530

    Update ConvexHull.h

--- Convex/headers/ConvexHull.h
+++ Convex/headers/ConvexHull.h
@@ -10,10 +10,6 @@ class ConvexHull
 
 
                   vector<Point2D> convexHull(vector<Point2D>&points);
-    //             vector<Point3D> convexHull(vector<Point3D>& points);
-    //             void findHull(Point3D p1, Point3D p2, vector<Point3D>& points, vector<Point3D>& hull);
-    //             double distance(Point3D p1, Point3D p2, Point3D p);
-    //             int orientation(Point3D p, Point3D q, Point3D r);
-    //             vector<Point3D>quickhull(vector<Point3D>& points); // public :
+ 
     private :
 };
