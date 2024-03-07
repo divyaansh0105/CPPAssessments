907b3cb4215003a95ee6405e5c684ea64ad176a5
divyaansh0105 <158050858+divyaansh0105@users.noreply.github.com>
2024-03-07 12:05:47 +0530

    Update Point2D.h

--- Convex/headers/Point2D.h
+++ Convex/headers/Point2D.h
@@ -14,8 +14,7 @@
         void setX(double x);
         void setY(double y);
         bool operator<(const Point2D &other) const;
-        
-        // bool checkPoint(Point3D p1);
+     
     private:
         double mX;
         double mY;
